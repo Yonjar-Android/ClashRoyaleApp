dcdb4cf3c540bcc9717df0894c4b5e7cd2109c05
Yonjar <juan132y@gmail.com>
2024-03-19 19:53:15 -0600

    player response modified

--- app/src/main/java/com/yonjar/clashroyalestats/data/modelResponses/PlayerResponse.kt
+++ app/src/main/java/com/yonjar/clashroyalestats/data/modelResponses/PlayerResponse.kt
@@ -1,46 +1,70 @@
 package com.yonjar.clashroyalestats.data.modelResponses
 
 import com.google.gson.annotations.SerializedName
+import com.yonjar.clashroyalestats.domain.models.BadgeModel
+import com.yonjar.clashroyalestats.domain.models.CardModel
 import com.yonjar.clashroyalestats.domain.models.PlayerMainInfoModel
 
 data class PlayerResponse(
-    @SerializedName("tag") val tagPlayer:String,
-    @SerializedName("name") val userName:String,
-    @SerializedName("expLevel") val level:Int,
-    @SerializedName("wins") val wins:Int,
-    @SerializedName("losses") val losses:Int,
-    @SerializedName("trophies") val trophies:Int,
-    @SerializedName("bestTrophies") val bestTrophies:Int,
+    @SerializedName("tag") val tagPlayer: String,
+    @SerializedName("name") val userName: String,
+    @SerializedName("expLevel") val level: Int,
+    @SerializedName("wins") val wins: Int,
+    @SerializedName("losses") val losses: Int,
+    @SerializedName("trophies") val trophies: Int,
+    @SerializedName("bestTrophies") val bestTrophies: Int,
     @SerializedName("badges") val badges: List<Badges>,
-    @SerializedName("cards") val cards:List<Card>,
-    @SerializedName("currentFavouriteCard") val favouriteCard:Card,
-    @SerializedName("currentDeck") val currentDeck:List<Card>
+    @SerializedName("cards") val cards: List<Card>,
+    @SerializedName("currentFavouriteCard") val favouriteCard: Card,
+    @SerializedName("currentDeck") val currentDeck: List<Card>
 ) {
     fun toPlayerModel() = PlayerMainInfoModel(
-        tagPlayer = tagPlayer, userName = userName, level = level, wins = wins, losses = losses,
-        currentTrophies = trophies, bestTrophies = bestTrophies, favouriteCard = favouriteCard.cardImage.medium,
-        currentDeck = currentDeck, cards, badges
+        tagPlayer = tagPlayer,
+        userName = userName,
+        level = level,
+        wins = wins,
+        losses = losses,
+        currentTrophies = trophies,
+        bestTrophies = bestTrophies,
+        favouriteCard = favouriteCard.cardImage.medium,
+        currentDeck = currentDeck,
+        cards,
+        badges
     )
 }
 
 data class Card(
-    @SerializedName("name") val name:String,
+    @SerializedName("name") val name: String,
     @SerializedName("level") val level: Int,
-    @SerializedName("rarity") val rarity:String,
-    @SerializedName("elixirCost") val elixir:Int,
-    @SerializedName("iconUrls") val cardImage:IconUrlsCard
-)
+    @SerializedName("rarity") val rarity: String,
+    @SerializedName("elixirCost") val elixir: Int,
+    @SerializedName("iconUrls") val cardImage: IconUrlsCard
+) {
+    fun toCardModel() = CardModel(
+        name = name,
+        level = level,
+        rarity = rarity,
+        elixirCost = elixir,
+        cardImage = cardImage.medium
+    )
+}
 
 data class IconUrlsCard(
-    @SerializedName("medium") val medium:String
+    @SerializedName("medium") val medium: String
 )
 
 data class Badges(
-    @SerializedName("name") val name:String,
+    @SerializedName("name") val name: String,
     @SerializedName("level") val level: Int,
-    @SerializedName("iconUrls") val image:IconUrlsBadge
-)
+    @SerializedName("iconUrls") val image: IconUrlsBadge
+) {
+    fun toBadgeModel() = BadgeModel(
+        name = name,
+        level = level,
+        image = image.badgeImage
+    )
+}
 
 data class IconUrlsBadge(
-    @SerializedName("large") val badgeImage:String
+    @SerializedName("large") val badgeImage: String
 )
