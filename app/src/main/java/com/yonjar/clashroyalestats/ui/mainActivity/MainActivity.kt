664241c6f606303234f11376c8f572ef2253242f
Yonjar <juan132y@gmail.com>
2024-03-21 08:58:29 -0600

    1- The app logo has been changed. 2- Splash Screen has been implemented. 3- Spanish strings have been added and some styles have been modified to look a better design.

--- app/src/main/java/com/yonjar/clashroyalestats/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/yonjar/clashroyalestats/ui/mainActivity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yonjar.clashroyalestats.ui.mainActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yonjar.clashroyalestats.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,12 +12,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-        binding.navMenu.visibility = View.GONE
+        splashScreen.setKeepOnScreenCondition { false }
 
     }
 }
